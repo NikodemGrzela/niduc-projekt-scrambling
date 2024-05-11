5b3279c7fed34a824cd8a66048f5ef6913e9eda5
Kamila <268265@student.pwr.edu.pl>
2024-05-12 01:19:38 +0200

    Remove menu from xor scrambler, one scrambler one class

--- NIDUC_Projekt/XORKeyScrambler.py
+++ NIDUC_Projekt/XORKeyScrambler.py
@@ -2,34 +2,15 @@ import random
 
 class XORKeyScrambler:
     def __init__(self):
-        self.key = None
-        self.signal = None
+        self.key = generate_key(256)
 
     def scramble(self, signal):
-        self.signal = signal
-        self.key = generate_key(256)
-        return self.choose() + ',' + self.key
-
-    def choose(self):
-        choice = 1
-        while choice != 0:
-            show_menu()
-            choice = input()
-            if choice.isnumeric():
-                choice = int(choice)
-
-            if choice == 1:
-                return self.xor_scramble()
-            else:
-                print("No such an option")
-
-    def xor_scramble(self):
         scrambled_signal = ""
         k = 0
-        for i in range(len(self.signal)):
+        for i in range(len(signal)):
 
             # XOR each bit of the signal with the corresponding bit of the key
-            if self.signal[i] == self.key[k]:
+            if signal[i] == self.key[k]:
                 scrambled_signal += '0'
             else:
                 scrambled_signal += '1'
@@ -38,32 +19,21 @@ class XORKeyScrambler:
                 k = 0
         return scrambled_signal
 
-    def xor_descramble(self,signal,key):
+    def descramble(self,signal):
         descrambled_signal = ""
         k = 0
 
         for i in range(len(signal)):
 
             # XOR each bit of the signal with the corresponding bit of the key
-            if signal[i] == key[k]:
+            if signal[i] == self.key[k]:
                 descrambled_signal += '0'
             else:
                 descrambled_signal += '1'
             k += 1
-            if k == len(key):
+            if k == len(self.key):
                 k = 0
         return descrambled_signal
-
-
-
-
-
-def show_menu():
-    print("1. XOR with key")
-
-
-
-
 
 def generate_key(signal):
     length = signal
