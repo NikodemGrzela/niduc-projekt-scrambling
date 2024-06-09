3a7011883aa9c8dfe9aa50dd8728c1472f66cffe
Bartosz <96384251+hisoroo@users.noreply.github.com>
2024-06-09 16:28:43 +0200

    Update Sender.py
    Tak na szybko, można to potem poprawić

--- NIDUC_Projekt/Sender.py
+++ NIDUC_Projekt/Sender.py
@@ -7,14 +7,17 @@ class Sender:
         self.signal_after_scrambling = None
         self.scrambler = scrambler
 
-    def send_data(self):
-        # Ask user for 0/1 amount, length
-        signal_length = int(input("Enter signal length:"))
-        percentage_ones = int(input("Enter percentage of ones: "))
-        self.data = self.generate_data(signal_length, percentage_ones)
+    def send_signal(self):
+        self.data = self.generate_signal()
         self.signal_after_scrambling = self.scrambler.scramble(self.data)
         return self.signal_after_scrambling
 
+    # metoda do testow
+    def send_data(self):
+        self.signal_after_scrambling = self.scrambler.scramble(self.data)
+        return self.signal_after_scrambling
+
+    # metoda do menu
     def print_data(self):
         print("Sender:")
         print("Generated signal: " + signal_to_string(self.data))
@@ -33,10 +36,20 @@ class Sender:
         random.shuffle(signal)
         return signal
     '''
-    
+    # metoda do testow
     def generate_data(self, signal_length, percentage_ones):
         number_of_ones = int(signal_length * percentage_ones / 100)
         number_of_zeros = signal_length - number_of_ones
+        signal = [1] * number_of_ones + [0] * number_of_zeros
+        random.shuffle(signal)
+        return signal
+        
+    # metoda do testow metoda do menu
+    def generate_signal(self):
+        # Ask user for 0/1 amount, length
+        signal_length = int(input("Enter signal length:"))
+        number_of_zeros = int(input("Enter number of zeros: "))
+        number_of_ones = signal_length - number_of_zeros
         signal = [1] * number_of_ones + [0] * number_of_zeros
         random.shuffle(signal)
         return signal
